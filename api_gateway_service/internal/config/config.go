ab1d13ddb4e8d9b5a20e9b04f4849c61cefbe051
Akzam Khamitov <makoron3002@gmail.com>
2024-05-08 15:46:52 +0500

    Added a config to the api_gateway_service.

---
+++ api_gateway_service/internal/config/config.go
@@ -0,0 +1,48 @@
+package config
+
+import (
+	"os"
+	"strconv"
+)
+
+type AppConfig struct {
+	Server       ServerConfig
+	UserService  UserServiceConfig
+	JwtSecretKey string
+}
+
+func NewAppConfig() (AppConfig, error) {
+	httpPortStr := os.Getenv("HTTP_PORT")
+	httpPort, err := strconv.Atoi(httpPortStr)
+	if err != nil {
+		return AppConfig{}, err
+	}
+
+	httpTimeoutSecondsStr := os.Getenv("HTTP_TIMEOUT_SECONDS")
+	httpTimeoutSeconds, err := strconv.Atoi(httpTimeoutSecondsStr)
+	if err != nil {
+		return AppConfig{}, err
+	}
+
+	userServicePortStr := os.Getenv("USER_SERVICE_PORT")
+	userServicePort, err := strconv.Atoi(userServicePortStr)
+	if err != nil {
+		return AppConfig{}, err
+	}
+
+	userServiceConfig := UserServiceConfig{
+		Host: os.Getenv("USER_SERVICE_HOST"),
+		Port: userServicePort,
+	}
+
+	serverConfig := ServerConfig{
+		HttpPort:           httpPort,
+		HttpTimeoutSeconds: httpTimeoutSeconds,
+	}
+
+	return AppConfig{
+		Server:       serverConfig,
+		UserService:  userServiceConfig,
+		JwtSecretKey: os.Getenv("JWT_SECRET_KEY"),
+	}, nil
+}
---
+++ api_gateway_service/internal/config/user_service.go
@@ -0,0 +1,6 @@
+package config
+
+type UserServiceConfig struct {
+	Host string
+	Port int
+}
